6bbfdb6331f3d046ee7069858af60bacc62f227c
Levi <shekhar.3706@gmail.com>
2026-08-15 08:37:39 +0530

    set port max limit

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,15 @@
-import * as z from 'zod'
-
+import * as z from 'zod/mini'
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(["dev", "test", "production"]).default('dev'),
-  PORT: z.coerce.number().int().positive(),
+  NODE_ENV: z._default(
+    z.enum(["dev", "test", "production"]),
+    'dev'
+  ),
+  PORT: z.coerce.number().check(
+    z.int(),
+    z.positive(),
+    z.maximum(65535),
+  ),
 });
 
 export const env = envSchema.parse(process.env);
